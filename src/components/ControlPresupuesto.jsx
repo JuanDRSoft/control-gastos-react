61ddc569da0d25bed535dd1496b115a04e9d1332
Juan Diego <juandirios2011@gmail.com>
2022-02-05 22:13:49 -0500

    First commit

---
+++ src/components/ControlPresupuesto.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+
+const ControlPresupuesto = ({ gastos, presupuesto }) => {
+  const [disponible, setDisponible] = useState(0);
+  const [gastado, setGastado] = useState(0);
+
+  useEffect(() => {
+    const totalGastado = gastos.reduce(
+      (total, gasto) => Number(gasto.cantidad) + Number(total),
+      0
+    );
+    console.log(totalGastado);
+    setGastado(totalGastado);
+  }, [gastos]);
+
+  const formatearCantidad = (cantidad) => {
+    return cantidad.toLocaleString("es-CO", {
+      style: "currency",
+      currency: "COP",
+    });
+  };
+  return (
+    <div className="contenedor-presupuesto contenedor sombra dos-columnas">
+      <div>
+        <p>Grafica aqui</p>
+      </div>
+      <div className="contenido-presupuesto">
+        <p>
+          <span>Presupuesto: </span> {formatearCantidad(presupuesto)}
+        </p>
+        <p>
+          <span>Disponible: </span> {formatearCantidad(disponible)}
+        </p>
+        <p>
+          <span>Gastado: </span> {formatearCantidad(gastado)}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ControlPresupuesto;
